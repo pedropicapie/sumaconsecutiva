7dcc6c61e029e72638613a7d2f6bf410087f2648
Adrian <Adrian@LAPTOP-172LSVQK>
2021-04-29 18:47:26 -0500

    ejercicio1

---
+++ src/sumaconsecutiva/Sumaconsecutiva.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sumaconsecutiva;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Sumaconsecutiva {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int suma = 0;
+        for(int i = 1; i <= 103; i++){
+            suma = suma + i;
+        }
+        System.out.println(suma);{
+        } 
+    }
+    
+}
